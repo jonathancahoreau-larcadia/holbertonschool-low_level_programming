3a804c8ed9eb6355d3a5a76a94e42aedcfd95630
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-24 14:53:30 +0100

    correctif betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*prints exactly "Programming is like building a multilingual puzzle*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
