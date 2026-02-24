3a19d4590c53f9603431b699c894fcd15b216df9
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-24 14:51:39 +0100

    écrire un programme pour afficher une phrase

---
+++ hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*prints exactly "Programming is like building a multilingual puzzle*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
