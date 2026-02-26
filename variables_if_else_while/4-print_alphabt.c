55d1d46f19d4e3db85478361e5b3db3bd7e165a8
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-26 13:57:36 +0100

    Exclure deux lettres

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,16 +9,16 @@ int main(void)
 
 	for (i = 97 ; i < 123 ; i++)
 
-	{
+
 		if (i == 101 || i == 113)
 		{
-		i;
+		continue;
 		}
 		else
 		{
 		putchar (i);
 		}
 	putchar ('\n');
-	}
+
 return (0);
 }
