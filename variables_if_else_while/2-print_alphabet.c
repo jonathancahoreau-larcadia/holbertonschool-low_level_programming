10721de718f21bab78863a895bbdedd01b107849
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-26 10:50:41 +0100

    utiliser putchar pour faire l'alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-int i;
-for (i = 97; i < 123; i++)
-{
-putchar (i);
-putchar ('\n');
-}
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar (i);
+	putchar ('\n');
+
 return (0);
 }
