ac1a6a1b85be810783c7e9dcc1e4a5ff9af4203b
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-28 14:39:07 +0100

    Afficher l'alphabet 10x

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print_alphabetx10
+ */
 
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	print_alphabet();
 	i++;
-
-	return (0);
 }
