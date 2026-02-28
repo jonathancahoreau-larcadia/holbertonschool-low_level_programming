a1bf7a25f02220de4883eed718c3bb40b814fc8f
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-28 20:11:08 +0100

    valeur absolue

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - donne la valeur absolu d'un nombre
+ * @n: est le nom de l'entier
+ *
+ * Return: renvoie un nombre positif dans tous les cas
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+return (n);
+}
