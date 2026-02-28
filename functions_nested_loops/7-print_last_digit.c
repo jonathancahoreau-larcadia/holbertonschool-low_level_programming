838017139b47d9365a3786294163e2cfb344beb2
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-28 20:59:33 +0100

    last digit toujours positif

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,16 @@
 #include "main.h"
+/**
+ * print_last_digit - fonction pour afficher le dernier chiffre
+ * @n: le nombre
+ *
+ * Return: renvoie le digit positif
+ */
 
 int print_last_digit(int n)
 {
 	int d = n % 10;
-	if (d < 0)
+		if (d < 0)
+
 	{
 		d = -d;
 		_putchar(d + '0');
