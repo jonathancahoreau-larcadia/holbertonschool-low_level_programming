025fb1e969c1a6d800bf87668085c79ebb402b63
Jonathan <jonathan.cahoreau@gmail.com>
2026-02-28 15:24:41 +0100

    implémentation de la fonction 1-alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print_alphabetx10
+ * print_alphabet - imprime l'alphabet en minuscules suivi d'un saut de ligne
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - imprime l'alphabet dix fois
  */
 
 void print_alphabet_x10(void)
