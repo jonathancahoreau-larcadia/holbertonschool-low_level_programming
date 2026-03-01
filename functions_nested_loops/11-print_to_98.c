49857a1c92e6e365adb8459ac936111a6195f437
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-01 12:53:42 +0100

    affiche des nombres jusqu'à un autre

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * print_to_98 - affiche les nombres jusqu'à 98
+ * @n: est le nombre de départ
+ *
+ * Return: aucun
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
