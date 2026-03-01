607749f286936a37a1b25df73e2e15b348b65786
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-01 01:10:22 +0100

    afficher compteur d'heures

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+ * jack_bauer - fonction pour afficher un compteur horaire
+ *
+ * Return: renvoie rien
+ */
 void jack_bauer(void)
 {
 	int i;
 	int h, m;
 
 	for (i = 0; i < 1440; i++)
-    	{
+		{
 		h = i / 60;
 		m = i % 60;
 
@@ -16,5 +20,5 @@ void jack_bauer(void)
 		_putchar('0' + (m / 10));
 		_putchar('0' + (m % 10));
 		_putchar('\n');
-    }
+		}
 }
