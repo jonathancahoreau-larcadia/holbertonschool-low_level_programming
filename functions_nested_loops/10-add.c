46ea9dd085ff71cf7f0b9ad5e5462746940c3d04
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-01 12:14:35 +0100

    fonction ajouter

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - ajoute deux entiers
+ * @a: un entier
+ * @b: un entier
+ *
+ * Return: renvoie l'addition des deux paramètres
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
