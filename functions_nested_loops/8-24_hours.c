b072f26a93f6be482d4bcbca4bef07b566413b0a
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-01 01:05:14 +0100

    afficher compteur d'heures

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int i;
+	int h, m;
+
+	for (i = 0; i < 1440; i++)
+    	{
+		h = i / 60;
+		m = i % 60;
+
+		_putchar('0' + (h / 10));
+		_putchar('0' + (h % 10));
+		_putchar(':');
+		_putchar('0' + (m / 10));
+		_putchar('0' + (m % 10));
+		_putchar('\n');
+    }
+}
