990135e7bc6bf255a1b8fc17d1ab196f57079989
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-03 00:19:14 +0100

    mise à jour

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,10 @@
 #include "main.h"
 
 /**
-*print_rev -affiche la chaine de caractere à l'envers puis retour à la ligne
+*print_rev - affiche la chaine de caractere à l'envers puis retour à la ligne
 *@s: est une chaine de caractère
-*_strlen: une fonction pour calculer la longueur d'une chiane
 *
 */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
-}
-
-
 void print_rev(char *s)
 {
 	int i;
