0b7f580768975b6ef7b3296b26c825d0e4ed8cd1
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-02 22:19:04 +0100

    attribuer un pointeur

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*reset_to_98 - remet à 98
+*@n: est un pointeur qui correspond à une valeur à modifier
+*
+*/
+
+void reset_to_98(int *n)
+{
+	int *ptr_i = n;
+	*ptr_i = 98;
+}
