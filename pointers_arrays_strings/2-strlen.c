7a69207eafa839ac9fe2ad3ebd852f37690ec2b1
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-02 23:30:54 +0100

    Calcul la longueur d'une chaine de caractere

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strlen - calcul la longeur d'une chaine
+*@s: est une chaine de caractère
+*
+*Return: renvoie le nombre de boucles jusqu'au caractère de fin '\0'
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
