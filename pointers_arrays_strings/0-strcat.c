ecc9b1060a1c6bb1709df9cda59e84d5a527e9e6
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-04 10:40:10 +0100

    fonction strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatener 2 chaines de caracteres
+ * @dest: un caractere
+ * @src: un caractere
+ *
+ * Return: renvoie l'adresse de dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = ('\0');
+
+return (dest);
+}
