c07510ff387da5f95f9b135c95eacc5b0b8d7b89
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-04 09:18:06 +0100

    maj apres checker

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-*rev_string - modifie la chaine de caractere à l'envers puis retour à la ligne
+*rev_string - modifie la chaine de caractere à l'envers ; retour à la ligne
 *@s: est une chaine de caractère
 *
 */
+
 void rev_string(char *s)
 {
 	int i = 0;
@@ -17,12 +18,10 @@ void rev_string(char *s)
 	}
 	for (i = i - 1; i > j; i--)
 	{
-
-
-			temp = s[j];
-			s[j] = s[i];
-			s[i] = temp;
-			j++;
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		j++;
 
 	}
 }
