e48a8d8cedba4348553641180e0c240a548459e4
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-04 09:14:50 +0100

    fonction atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_atoi - transforme une chaine de caractere en integer
+*@s: un caractère
+*
+*Return: renvoie un int = result * sign
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+	int result = 0;
+	int sign = 1;
+	int find = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			find = 1;
+		}
+		else if (find == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	return (result * sign);
+}
