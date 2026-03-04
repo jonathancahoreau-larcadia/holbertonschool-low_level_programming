756cac28219fe0a83b21e1af7603fd1a98d74297
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-05 00:26:30 +0100

    ajout addition

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -9,6 +9,8 @@ int main(void)
 {
 	int add = 1, subtract = 2, multiply = 3, divide = 4, quit = 0;
 	int choice = 0;
+	int number_a = 0, number_b = 0;
+	int result_add;
 
 	do {
 		printf("Simple Calculator\n");
@@ -20,17 +22,27 @@ int main(void)
 		printf("Choice: ");
 		scanf("%d", &choice);
 
-		if (choice != 0)
+		if (choice == add)
+		{
+			printf("A: ");
+			scanf("%d", &number_a);
+			printf("B: ");
+			scanf("%d", &number_b);
+
+			result_add = number_a + number_b;
+			printf("Result: %d\n", result_add);
+		}
+
+		if (choice != quit)
 		{
 			printf("Invalid choice\n");
 			printf("Choice: ");
 			scanf("%d", &choice);
 		}
-	} while (choice != 0);
+
+	} while (choice != quit);
 	{
 		printf("Bye!\n");
 	}
-
-
 return (0);
 }
