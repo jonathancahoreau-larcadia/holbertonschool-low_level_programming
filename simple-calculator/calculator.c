e059a359b9663f05a43bd21b354b4756c56a02c6
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-04 21:49:04 +0100

    squelette calculatrice

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *
+ *
+*/
+
+int main(void)
+{
+	int a =1 , b = 2, c = 3, d = 4, e = 0;
+	int choice = 0;
+
+	printf("Simple Calculator\n");
+	printf("%d) Add\n", a);
+	printf("%d) Subtract\n", b);
+	printf("%d) Multiply\n", c);
+	printf("%d) Divide\n", d);
+	printf("%d) Quit\n", e);
+
+	printf("Choice: ");
+	scanf("%d", &choice);
+
+	if (choice == 0)
+	{
+	printf("Bye!\n");
+	}
+
+return (0);
+}
