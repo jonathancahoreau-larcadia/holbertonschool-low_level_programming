74ccdbd51f1ea432eed5568a9f242567b80cc30a
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-04 23:57:41 +0100

    modif réinit

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 /**
+ *main - Entry point
  *
- *
+ *Return: Always (0) (Success)
 */
 
 int main(void)
 {
-	int a =1 , b = 2, c = 3, d = 4, e = 0;
+	int add = 1, subtract = 2, multiply = 3, divide = 4, quit = 0;
 	int choice = 0;
 
-	printf("Simple Calculator\n");
-	printf("%d) Add\n", a);
-	printf("%d) Subtract\n", b);
-	printf("%d) Multiply\n", c);
-	printf("%d) Divide\n", d);
-	printf("%d) Quit\n", e);
-
-	printf("Choice: ");
-	scanf("%d", &choice);
-
-
-	while (choice > 4)
-	{
-		printf("Invalid choice\n");
+	do {
+		printf("Simple Calculator\n");
+		printf("%d) Add\n", add);
+		printf("%d) Subtract\n", subtract);
+		printf("%d) Multiply\n", multiply);
+		printf("%d) Divide\n", divide);
+		printf("%d) Quit\n", quit);
 		printf("Choice: ");
 		scanf("%d", &choice);
-		if (choice == 0)
+
+		if (choice != 0)
 		{
-			printf("Bye!\n");
-			return (0);
+			printf("Invalid choice\n");
+			printf("Choice: ");
+			scanf("%d", &choice);
 		}
-		}
+	} while (choice != 0);
+	{
+		printf("Bye!\n");
+	}
+
 
 return (0);
 }
