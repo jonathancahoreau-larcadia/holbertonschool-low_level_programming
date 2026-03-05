00a545e38600c37955e70b00cf62701b216ab4cc
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-05 13:26:49 +0100

    fonction memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,10 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * _memset - remplace la valeur de la memoire par une autre valeur
+ * @s: pointeur vers la memoire qui attribue une valeur
+ * @b: est la chaine de caractere que l'on utilise pour remplacer
+ * @n: entier positif pour remplacer n fois
+ *
+ * Return: la racine ptr de la chaine
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *ptr = s;
-
 
 	while (n != 0)
 	{
@@ -13,48 +19,4 @@ char *_memset(char *s, char b, unsigned int n)
 		n--;
 	}
 return (ptr);
-}
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
-
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
-    return (0);
 }
