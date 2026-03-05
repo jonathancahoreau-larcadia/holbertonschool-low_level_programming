d4cd9f8e7ec6d1d95c87fafe2c4d3f6e3ad9062c
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-05 01:16:30 +0100

    ajout division

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -10,7 +10,9 @@ int main(void)
 	int add = 1, subtract = 2, multiply = 3, divide = 4, quit = 0;
 	int choice = 0;
 	int number_a = 0, number_b = 0;
+	float fnumber_a = 0, fnumber_b = 0;
 	int result = 0;
+	float fresult = 0;
 
 	do {
 		printf("Simple Calculator\n");
@@ -51,6 +53,23 @@ int main(void)
 
 			result = number_a * number_b;
 			printf("Result: %d\n\n", result);
+		}
+		else if (choice == divide)
+		{
+			printf("A: ");
+			scanf("%f", &fnumber_a);
+			printf("B: ");
+			scanf("%f", &fnumber_b);
+
+			if (fnumber_b == 0)
+			{
+				printf("Error: division by zero\n\n");
+			}
+			else
+			{
+			fresult = fnumber_a / fnumber_b;
+			printf("Result: %.1f\n\n", fresult);
+			}
 		}
 
 		else if (choice != quit && choice > 4)
