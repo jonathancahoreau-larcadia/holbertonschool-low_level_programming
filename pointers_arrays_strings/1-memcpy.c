016e3d5c6ca063bf48a6420a9d672e412b28ab1e
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-05 14:43:30 +0100

    fonction memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ * _memcpy - copie la vzleur de la memoire n fois
+ * @dest: pointeur sur chaine de char
+ * @src: pointeur sur élément à copier
+ * @n: nombre de copies
+ *
+ * Return: (dest)renvoie l'adresse du pointeur sauvegardé en dbt de ligne
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *p = dest;
