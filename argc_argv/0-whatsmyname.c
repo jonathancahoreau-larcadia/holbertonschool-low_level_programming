5995ea3f10f76b2e4d5264e05fc1a31bcfd59f35
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-06 20:11:22 +0100

    affiche le programme

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - affiche le nom du programme
+ * @argc: nombre d'arg
+ * @argv: tableau d'arg
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	
+return (0);
+}
