d5f7e17b3957c92f7b8f5cd50747aaa2b153e273
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-06 10:21:46 +0100

    fonction strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,18 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			s = (s + i);
 			return (s);
 		}
+	s++;
 	}
-return (NULL);
+		if (c == '\0')
+		{
+			return (s);
+		}
+	return (NULL);
+
 }
