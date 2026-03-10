60bc82fa7470cc2f70f909fb5a0ee3168b407c99
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-10 15:14:36 +0100

    tableau de valeur croissante

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * array_range - tableau de valeur croissante
+ * @min: valeur minimum
+ * @max: valeur max
+ *
+ * Return: (pt) l'adresse correspondant au tableau
+ */
+
+int *array_range(int min, int max)
+{
+	int *pt = NULL;
+	int i;
+
+	if (min > max)
+	return (NULL);
+
+	pt = malloc((max - min + 1) * (sizeof(int)));
+	if (pt == NULL)
+	return (NULL);
+
+	for (i = 0; i <= max; i++)
+	{
+		pt[i] = (min + i);
+	}
+return (pt);
+}
