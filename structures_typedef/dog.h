4a15c2c2b207f33843a8cf1a0d62eadba10297fd
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-11 08:14:21 +0100

    correction betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - infos sur chien
+ * @name: nom du chien
+ * @age: age du chien
+ * @owner: nom du propriétaire
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
