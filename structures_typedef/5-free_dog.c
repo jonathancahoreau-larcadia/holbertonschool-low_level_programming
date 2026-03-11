bc6ac4c6e3dbf76377ee661e3f270be3d185b90c
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-11 13:05:31 +0100

    fonction libérer la mémoire

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * free_dog - libère la mémoire de la structure dog_t
+ * @d: pointeur vers la structure
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 	return;
-	
+
 	free(d->owner);
 	free(d->name);
 	free(d);
