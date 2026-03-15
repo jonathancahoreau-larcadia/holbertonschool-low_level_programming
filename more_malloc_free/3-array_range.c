40b8ab45ce9f864367b45f71bfff0e6cf8f4252c
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-15 12:15:54 +0100

    modification condition d'arrêt de i dans la boucle

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	if (pt == NULL)
 	return (NULL);
 
-	for (i = 0; i <= (max - min + 1); i++)
+	for (i = 0; i < (max - min + 1); i++)
 	{
 		pt[i] = (min + i);
 	}
