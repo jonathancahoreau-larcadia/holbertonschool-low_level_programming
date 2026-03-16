05d6cce09876e0ef4cd34db3ef1eed3b8c1ee814
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-16 11:55:42 +0100

    ecriture d'une fonction

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - fonction pour imprimer un nom
  * @name: pointeur vers un caractère
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-		f(name);
+	if(f != NULL)
+	f(name);
 }
