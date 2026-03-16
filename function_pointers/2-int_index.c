7fecca2f697b01b812536048cb3c5dfc4964a19b
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-16 16:07:04 +0100

    fonction comparative

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,17 +10,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
-
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
+		int i = 0;
 
-		if (cmp(array[i]))
-		return (i);
+		if (size <= 0)
+			return (-1);
 
+		for (i = 0; i < size; i++)
+		{
+
+			if (cmp(array[i]))
+			return (i);
+
+		}
 	}
 return (-1);
 }
