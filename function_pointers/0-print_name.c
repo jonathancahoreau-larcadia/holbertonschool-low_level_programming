dd2a8157f166de97a2c5c9f2b4da0899907e3fb9
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-17 10:18:45 +0100

    changement condition if

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,6 +4,8 @@
  * print_name - fonction pour imprimer un nom
  * @name: pointeur vers un caractère
  * @f: pointeur vers une fonction prenant un char * en argument
+ *
+ * Return: rien
  */
 void print_name(char *name, void (*f)(char *))
 {
