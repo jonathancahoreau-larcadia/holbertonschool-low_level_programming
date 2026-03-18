133221778b4320572b4b81fbd50ee99ab795733d
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-18 20:02:35 +0100

    calculatrice

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "3-calc.h"
+/**
+ * main - point d'entree
+ * @argc: le nombre d'arguments
+ * @argv: chaque chaine de caracteres
+ *
+ * Return: (O) si succes
+ */
 int main(int argc, char *argv[])
 {
+	int num1;
+	int num2;
+	int res = 0;
+	int (*op_func)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	op_func = get_op_func(argv[2]);
+	res = op_func(num1, num2);
+
+	printf("%d\n", res);
 
 return (0);
 }
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+
+/**
+ * op_add - addition
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de l'addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - soustraction
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de la soustraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de la multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de la division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le reste de la division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
