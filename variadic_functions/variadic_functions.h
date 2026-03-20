512f351d4e9a9623414d87931d4faba79ef87cb1
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-20 17:31:23 +0100

    miniprintf

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,4 +4,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct capture - Struct capture
+ *
+ * @char: la lettre à comparer
+ * @call_func: le pointeur sur fonction
+ */
+typedef struct capture
+{
+	char letter;
+	void (*call_func)(va_list *args);
+}
+capt_t;
 #endif
