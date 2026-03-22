fde710564e5836c40190f90a76478022e6dc5605
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-22 15:58:26 +0100

    assembleur linux64

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,15 @@
+section .data
+hworld db "Hello, World", 10
+longueur equ $-hworld
+
+section .text
+global _start
+_start :
+mov rax, 1
+mov rdi, 1
+mov rsi, hworld
+mov rdx, longueur
+syscall
+mov rax, 60
+mov rdi, 0
+syscall
