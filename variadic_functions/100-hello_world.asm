800bc88d9e435c754ef02f37009bb0edc7bc9934
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-22 16:33:12 +0100

    installation compileur pour arm64

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,19 @@
+default rel
+
 section .data
 hworld db "Hello, World", 10
 longueur equ $-hworld
 
 section .text
 global _start
-_start :
-mov rax, 1
-mov rdi, 1
-mov rsi, hworld
-mov rdx, longueur
-syscall
-mov rax, 60
-mov rdi, 0
-syscall
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, hworld
+	mov rdx, longueur
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
