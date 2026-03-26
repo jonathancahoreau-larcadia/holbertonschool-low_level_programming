36fcdcfb5002589acce197eb1eae41722f052b7f
Jonathan <jonathan.cahoreau@gmail.com>
2026-03-26 10:52:46 +0100

    ajout d'un noeud

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - pointeur sur fonction qui retourne une structure
+ * @head: pointeur sur pointeur qui retourne l'adresse du head
+ * @str: pointeur qui renvoie une chaine de caractere.
+ *
+ * Return: (NULL) si probleme memoire, (new) l'adresse du noeud ajouté
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+return (new);
+}
