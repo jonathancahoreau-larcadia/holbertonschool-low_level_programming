2c5b36b4bf4744bb1b1c19ddd8decbd326cec5bf
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-03 12:19:53 +0200

    effacer des noeuds dans une liste chainées double

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * delete_dnodeint_at_index - efface les noeuds
+ * @head: pointeur qui pointe sur le pointeur head
+ * @index: le numéro du noeud
  *
+ * Return: (-1)si echec, (1) si succes
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -30,17 +34,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 		*head = tmp->next;
 		tmp->next->prev = NULL;
-		free (tmp);
+		free(tmp);
 		return (1);
 	}
 	if (tmp->next == NULL)
 	{
 		tmp->prev->next = NULL;
-		free (tmp);
+		free(tmp);
 	return (1);
 	}
 	tmp->prev->next = tmp->next;
 	tmp->next->prev = tmp->prev;
-	free (tmp);
-return (1);
+	free(tmp);
+	return (1);
 }
+
