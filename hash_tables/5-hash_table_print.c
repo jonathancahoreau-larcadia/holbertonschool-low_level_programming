1d99044038e2feb616ca1cac692e52349c4b8894
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-05 23:27:54 +0200

    afficher les pairs des hashtables

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - afficher les hashtable
+ * @ht: pointeur sur la hashtable
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	int virg = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node)
+		{
+			if (virg == 1)
+				printf(", ");
+			printf("%s : %s", node->key, node->value);
+			virg = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
