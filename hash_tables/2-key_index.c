7810b09d4a1faa3e584be6044fc3492a19c5b03a
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-05 15:53:56 +0200

    index d'une clé hashé par djb2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -12,10 +12,11 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+
 	unsigned long int index = 0;
 	unsigned long int hash = 0;
 
 	hash = hash_djb2(key);
-	index = hash * size;
+	index = hash % size;
 return (index);
 }
