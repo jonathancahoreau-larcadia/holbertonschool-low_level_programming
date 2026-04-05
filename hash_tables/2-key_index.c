f0a322aabe8b92debe567001cf0b36a1f64eaa4b
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-05 15:52:24 +0200

    index d'une clé hashé par djb2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * key_index - formule pour attribuer une clé
+ * @key: pointeur sur un char
+ * @size: pointeur sur un entier toujours positif
+ *
+ * Return: (index) retoune la clé indexée
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = 0;
+	unsigned long int hash = 0;
+
+	hash = hash_djb2(key);
+	index = hash * size;
+return (index);
+}
