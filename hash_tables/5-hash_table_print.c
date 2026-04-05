ee432fd52bc4362702c42f00bdfe0b52398c47c7
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-05 23:30:19 +0200

    modif apostrophe

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (virg == 1)
 				printf(", ");
-			printf("%s : %s", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			virg = 1;
 			node = node->next;
 		}
