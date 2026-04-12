3a11cf2f164a462c4de3a6a67d7c8ed7192fe481
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-13 00:49:53 +0200

    ajout de fichiers text dans un fichier

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,8 +25,6 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == (-1))
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	if (fchmod(fd_to, 0664) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	buffer = malloc((1024 * sizeof(char)));
 	if (!buffer)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
