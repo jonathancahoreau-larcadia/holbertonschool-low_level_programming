950cdacda57dcb2914cd2b700af76607140f9a5d
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-13 00:40:51 +0200

    ajout de fichiers text dans un fichier

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	if (fd_from == (-1))
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 		argv[1]), exit(98);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == (-1))
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 	buffer = malloc((1024 * sizeof(char)));
