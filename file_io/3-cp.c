a9677cbdc60bcda60e8ff56c37c0fd392854d995
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-13 00:45:13 +0200

    ajout de fichiers text dans un fichier

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,27 +20,27 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == (-1))
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n",
 		argv[1]), exit(98);
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == (-1))
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	buffer = malloc((1024 * sizeof(char)));
 	if (!buffer)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	r = read(fd_from, buffer, 1024);
 	if (r == (-1))
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n",
 		argv[1]), free(buffer), exit(98);
 	while (r > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w != r)
 			free(buffer), dprintf(STDERR_FILENO,
-			 "Error: Can't write to file %s\n", argv[2]), exit(99);
+			 "Error: Can't write to %s\n", argv[2]), exit(99);
 		r = read(fd_from, buffer, 1024);
 		if (r == (-1))
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n",
 			argv[1]), free(buffer), exit(98);
 	}
 	free(buffer);
